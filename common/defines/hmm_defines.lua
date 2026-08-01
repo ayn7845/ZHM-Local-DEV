825631799140da38b9737b5ede39f21d2ca66b7c
d m <dominickingbruh2@gmail.com>
2026-08-01 03:14:40 -0700

    Update hmm_defines.lua

--- common/defines/hmm_defines.lua
+++ common/defines/hmm_defines.lua
@@ -63,7 +63,7 @@ NDefines.NNavy.NAVAL_TRANSFER_PRIORITY = 1								-- Default convoy priority for
 NDefines.NNavy.SUPPLY_PRIORITY = 2								    	-- Default convoy priority for supplying units via sea
 NDefines.NNavy.RESOURCE_ORIGIN_PRIORITY = 3								-- Default convoy priority for resources shipped internally
 NDefines.NNavy.RESOURCE_EXPORT_PRIORITY = 4								-- Default convoy priority for export trade
-NDefines.NNavy.RESOURCE_LENDLEASE_PRIORITY = 5                          -- Default convoy priority for export lend lease
+NDefines.NNavy.RESOURCE_LENDLEASE_PRIORITY = 3                          -- Default convoy priority for export lend lease
 NDefines.NCountry.FUEL_LEASE_CONVOY_RATIO = 0.001
 
 
